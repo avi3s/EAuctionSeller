cec6994c96479911f4052d9485b7ebd5fc2fa8ca
avi3s <avirup.pal@gmail.com>
2022-08-23 23:42:16 +0530

    first commit

---
+++ src/main/java/com/eauction/seller/EauctionSellerApplication.java
@@ -0,0 +1,13 @@
+package com.eauction.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EauctionSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EauctionSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eauction/seller/EauctionSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eauction.seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EauctionSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
